554b00dde27aaa46763bce717329511dfb0dd9ac
jayadamsmorgan <applejay1802@gmail.com>
2024-01-05 23:00:01 -0500

    Update Vapor to 4.90.0

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.89.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.90.0"),
         // 🗄 An ORM for SQL and NoSQL databases.
         .package(url: "https://github.com/vapor/fluent.git", from: "4.8.0"),
         // 🪶 Fluent driver for SQLite.
